50e314656b1433138eac1bf200b6f560e3d39d66
Nishant <Nishant@example.com>
2025-05-26 13:57:53 +0000

    Resolved failed to read dockerfile

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Server is running');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
